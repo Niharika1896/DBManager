5340d5f996639211bcb8298c0cdd2ad2b5e66ab8
Niharika Sinha <78908196+Niharika1896@users.noreply.github.com>
2022-03-24 16:39:59 -0400

    Create Operation.java

---
+++ src/Operation/Operation.java
@@ -0,0 +1,14 @@
+/**
+ Operation.java
+ Purpose: To create operation class
+ @author Niharika Sinha (ns4451)
+ */
+
+package Operation;
+import Transaction.Transaction;
+
+public abstract class Operation {
+    public String variable;
+    public Transaction transaction;
+    public int operation_time;
+}
