92bd1088ff3c4a7c526cefe12e77aefc4b1ffdcf
Niharika Sinha <78908196+Niharika1896@users.noreply.github.com>
2022-03-24 16:41:14 -0400

    Add files via upload

---
+++ src/Lock/LockType.java
@@ -0,0 +1,12 @@
+/**
+ LockType.java
+ Purpose: To create LockType enum for the lock class
+ @author Niharika Sinha (ns4451)
+ */
+package Lock;
+
+public enum LockType {
+    READ_LOCK,
+    WRITE_LOCK,
+    NO_LOCK
+}
